fb94b360daf40aa24199b3134a6bdeb4678de829
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-15 13:00:11 +0900

    Title: 요일 맞추기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240115/요일 맞추기/guess-day-of-week.py
+++ 240115/요일 맞추기/guess-day-of-week.py
@@ -1,19 +1,16 @@
-m = [31,28,31,30,31,30,31,31,30,31,30,31]
+m_total = [31,28,31,30,31,30,31,31,30,31,30,31]
 weekday = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun']
 
 m1, d1, m2, d2 = map(int,input().split())
 
-if m2>m1:
-    total_day=m[m1-1]-d1+d2
-    for i in range(m1+1,m2):
-        total_day+=m[i]
-elif m1>m2:
-    total_day=m[m2-1]-d2+d1
-    for i in range(m2+1,m1):
-        total_day+=m[i]
-else:
-    total_day=d2-d1
-        
+daysum=0
 
-# print(total_day)
-print(weekday[-(abs(total_day)%7)])
+if m2>m1:
+    daysum = sum(m_total[m1:m2-1])
+elif m2<m1:
+    daysum = sum(m_total[m2:m1-1])
+
+if d2>d1:
+    print(weekday[(daysum+d2-d1)%7])
+elif d2<d1:
+    print(weekday[-(daysum+d1-d2)%7])
