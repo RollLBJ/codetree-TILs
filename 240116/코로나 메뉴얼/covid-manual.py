c5fb6b8a1f0652954315697e265ed8f25b3c028f
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-16 21:36:23 +0900

    Title: 코로나 메뉴얼, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240116/코로나 메뉴얼/covid-manual.py
+++ 240116/코로나 메뉴얼/covid-manual.py
@@ -1,6 +1,6 @@
-a = map(int,input().split())
-b = map(int,input().split())
-c = map(int,input().split())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+c = list(map(int,input().split()))
 
 cnt=0
 for t in [a,b,c]:
