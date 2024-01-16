bbcfb1fe24522a2c130be0047179d15d5204304f
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-16 21:57:54 +0900

    Title: 대문자로 바꾸기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240116/대문자로 바꾸기/change-to-capital.py
+++ 240116/대문자로 바꾸기/change-to-capital.py
@@ -3,7 +3,7 @@ def up(t):
     return t.upper()
 t = [tuple(map(up,input().split())) for _ in range(5)]
 
-for i in range:
-    for j in range:
+for i in t:
+    for j in i:
         print(j,end=" ")
     print('')
