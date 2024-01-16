26a6607d7d9be2e3f04e4b76432e8cebaaf1d7f8
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-16 21:38:06 +0900

    Title: 코로나 메뉴얼, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240116/코로나 메뉴얼/covid-manual.py
+++ 240116/코로나 메뉴얼/covid-manual.py
@@ -1,10 +1,10 @@
-a = list(map(int,input().split()))
-b = list(map(int,input().split()))
-c = list(map(int,input().split()))
+a = list(input().split())
+b = list(input().split())
+c = list(input().split())
 
 cnt=0
 for t in [a,b,c]:
-    if t[0] == 'Y' and t[1]>=37:
+    if t[0] == 'Y' and int(t[1])>=37:
         cnt+=1
 if cnt>=2:
     print("E")
