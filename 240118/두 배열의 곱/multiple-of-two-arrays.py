f3299d39e8b00860d99acb3d5730373780d67f33
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-18 12:48:43 +0900

    Title: 두 배열의 곱, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/두 배열의 곱/multiple-of-two-arrays.py
+++ 240118/두 배열의 곱/multiple-of-two-arrays.py
@@ -1,8 +1,9 @@
 a = [list(map(int,input().split())) for _ in range(3)]
+_t=input()
 b = [list(map(int,input().split())) for _ in range(3)]
 
 
 for i in range(3):
     for j in range(3):
-        print(f"{a[i][j]*b[i][j]} ")
+        print(f"{a[i][j]*b[i][j]}", end=' ')
     print("")
