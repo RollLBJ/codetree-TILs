e23265dd21badbed7060063679da1f0d74893c9a
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-19 21:34:58 +0900

    Title: 파스칼의 삼각형, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240119/파스칼의 삼각형/pascal's-triangle.py
+++ 240119/파스칼의 삼각형/pascal's-triangle.py
@@ -6,13 +6,13 @@ for i in range(1,n):
     for j in range(1,n):
         if j<=i:
             result[i][j] = result[i-1][j-1] + result[i-1][j]
-        else:
-            result[i][j]=0
 
 
 
 for i in range(n):
     for j in range(n):
-        if j<=i:
+        if j<i:
             print(result[i][j], end=' ')
+        elif j==i:
+            print(1, end=' ')
     print()
