11ebd0e58f77ac1c8cc9114327442c11424ffcaa
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-19 15:29:08 +0900

    Title: 격자 반대로 채우기, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/격자 반대로 채우기/grid-anti-fill.py
+++ 240119/격자 반대로 채우기/grid-anti-fill.py
@@ -1,10 +1,11 @@
 n= int(input())
+
 result=[[0]*n for _ in range(n)]
 cnt=1
-
+flag = 1 if n%2==0 else 0
 for i in range(n-1,-1,-1):
     for j in range(n-1,-1,-1):
-        if i%2==1:
+        if i%2==flag:
             result[j][i]=cnt
         else:
             result[n-1-j][i]=cnt
