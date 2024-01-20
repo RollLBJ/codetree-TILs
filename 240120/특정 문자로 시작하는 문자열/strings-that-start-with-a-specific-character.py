6946c9533b2c8358624406726e5b9f4039ddb9b3
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-20 21:55:38 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240120/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = [input() for _ in range(n)]
+char = input()
+
+total = 0
+cnt = 0
+for i in a:
+    if i.startswith(char):
+        total += len(i)
+        cnt += 1
+
+print(total / cnt if cnt > 0 else 0)
