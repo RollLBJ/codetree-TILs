b91f335bd029fb14550381bcaafb8b5f84105e37
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-22 20:00:12 +0900

    Title: 정수만 추출하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240122/정수만 추출하기/extract-only-integers.py
+++ 240122/정수만 추출하기/extract-only-integers.py
@@ -3,9 +3,10 @@ ans = []
 for num, i in enumerate(a):
     if not(i.isnumeric()):
        ans.append(a[:num])
+else:
+    ans.append(a)
 
 for num, i in enumerate(b):
     if not(i.isnumeric()):
        ans.append(b[:num])
-print(ans)
 print(int(ans[0]) + int(ans[1]))
