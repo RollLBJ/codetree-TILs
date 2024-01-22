67160b706daa72abadbd0f7c9f2e3c55ee08867c
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-22 13:10:37 +0900

    Title: 문자열 계속 지우기, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/문자열 계속 지우기/keep-removing-string.py
+++ 240122/문자열 계속 지우기/keep-removing-string.py
@@ -2,21 +2,9 @@ a = input()
 b = input()
 
 while(True):
-    for i in range(len(a)):
-        if a[i]==b[0]:
-            for j in range(len(b)):
-                _diff = 0
-                if (i+j) < len(a):
-                    if a[i+j] != b[j]:
-                        # print('다름')
-                        _diff=1
-                else:
-                    _diff=1
-            if _diff==0:
-                # print('발견')
-                a = a[:i] + a[i+len(b):]
-                break
-    
-    if _diff==1:
+    if a.find(b) != -1:
+        a = a[:a.find(b)] + a[a.find(b)+len(b):]
+    else:
         break
+
 print(a)
