b5543fe363bfd8307464f3da84359536e866bd76
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-22 20:59:11 +0900

    Title: 그 계절, 그 날, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240122/그 계절, 그 날/that-season-that-day.py
+++ 240122/그 계절, 그 날/that-season-that-day.py
@@ -13,13 +13,13 @@ month = [31,28,31,30,31,30,31,31,30,31,30,31]
 if is_yoon(y):
     month[1]=29
 
-if m>=1 and m<=12:
+if m>=1 and m<=12 and y>=1 and y<=3000:
     if d>=1 and d<=month[m-1]:
-        if month in [3, 4, 5]:
+        if m in [3, 4, 5]:
             print("Spring")
-        elif month in [6, 7, 8]:
+        elif m in [6, 7, 8]:
             print("Summer")
-        elif month in [9, 10, 11]:
+        elif m in [9, 10, 11]:
             print("Fall")
         else:
             print("Winter")
