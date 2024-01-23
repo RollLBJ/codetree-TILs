0198d51c2777477a176fbb300f7496b5477570b4
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-23 12:13:46 +0900

    Title: 2개 이상의 알파벳, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240123/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240123/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -5,8 +5,9 @@ for i in a:
     if i not in cnt:
         cnt.append(i)
     else:
-        print('Yes')
-        if b_cnt==1: break
+        if b_cnt==1: 
+            break
+            print('Yes')
         b_cnt+=1
 else:
     print('No')
