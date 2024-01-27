0cb9195fe73d784c85bb8e77f654bdb66fc711b0
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-01-27 20:14:56 +0900

    Title: 중앙값 계산 2, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240127/중앙값 계산 2/get-median-2.py
+++ 240127/중앙값 계산 2/get-median-2.py
@@ -1,7 +1,9 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-for num, i in enumerate(a):
-    if i%2 == 1:
-        _temp = a[:num+1]
-        print(_temp[len(_temp)//2 if len(_temp)%2 == 1 else len(_temp)//2-1], end=' ')
+for index, number in enumerate(a):
+    if (index+1)%2 == 1:
+        _temp = a[:index+1]
+        _temp.sort()
+        median = _temp[len(_temp)//2] if len(_temp)%2 == 1 else (_temp[len(_temp)//2-1]+_temp[len(_temp)//2])/2
+        print(median, end=' ')
