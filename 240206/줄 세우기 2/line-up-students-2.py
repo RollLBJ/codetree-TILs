268aa1ea4d27bfcbf6138bf100d72441d97b170e
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-02-06 13:23:05 +0900

    Title: 줄 세우기 2, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 240206/줄 세우기 2/line-up-students-2.py
+++ 240206/줄 세우기 2/line-up-students-2.py
@@ -4,7 +4,7 @@ a = [list(map(int,input().split())) for _ in range(n)]
 for num, i in enumerate(a):
     i.append(num+1)
 
-new = sorted(a, key=lambda a:(a[0],a[1]))
+new = sorted(a, key=lambda a:(a[0],-a[1]))
 
 for row in new:
     print(*row)
