4d6c3e80b5ed358946f41a4105ab78151b9dd40f
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-02-08 16:42:00 +0900

    Title: 블럭쌓는 명령2, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240208/블럭쌓는 명령2/block-stacking-commands2.py
+++ 240208/블럭쌓는 명령2/block-stacking-commands2.py
@@ -3,6 +3,6 @@ A = [list(map(int, input().split())) for _ in range(K)]
 result = [0] * N
 for cmd in A:
   for i in range(cmd[0], cmd[1]+1):
-    result[i] += 1
+    result[i-1] += 1
 
 print(max(result))
