e14bd35990b63308d95e541c6ce355a339b69de9
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-02-09 11:00:59 +0900

    Title: 최대로 겹치는 지점, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240209/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,10 @@
+N = int(input())
+A = [list(map(lambda x : int(x)+100, input().split())) for _ in range(N)]
+
+result = [0]*200
+
+for cmd in A:
+  for i in range(cmd[0], cmd[1]+1):
+    result[i-1] += 1
+
+print(max(result))
