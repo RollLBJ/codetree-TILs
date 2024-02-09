6b01c2bc59806169eaf679084bb1e3f62c6669a2
RollLBJ <103201836+RollLBJ@users.noreply.github.com>
2024-02-09 11:22:03 +0900

    Title: 왔다 갔던 구역 2, Time: 140ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240209/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240209/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -4,24 +4,31 @@ A = [list(map(lambda x: x if x.isalpha() else int(x), input().split())) for _ in
 result = [0]*2000
 
 index = 1000
-# result[index] = 1
+result[index] = 1
+
 for cmd in A:
-  if cmd[1] == 'R':
-    for i in range(cmd[0]):
-      result[index] += 1
+  result[index] += 1
+  for _ in range(cmd[0]):
+    if cmd[1] == 'R':
       index += 1
-  elif cmd[1] == 'L':
-    for i in range(cmd[0]):
-      result[index] += 1
+    elif cmd[1] == 'L':
       index -= 1
+    result[index] += 1
+
 cnt=0
 _flag = False
 # print(result)
 for i in result:
-  # if i>=1:/
+  # if i>=1:
     # print(i, end=' ')
+  
   if i>=2:
     cnt+=1
+    _flag = True
+  if i<=1 and _flag==True:
+    cnt-=1
+    _flag = False
+  
   
 
 print(cnt)
